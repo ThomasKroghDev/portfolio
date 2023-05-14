1194ab897d9da04728e8a38fae9814b8a7aa62da
Thomas Krogh <thomaskrogh@Thomass-Air.attlocal.net>
2023-05-13 22:59:43 -0500

    update use form

--- src/common/hooks/useForm.tsx
+++ src/common/hooks/useForm.tsx
@@ -1,10 +1,10 @@
-import { useState } from 'react';
+import { useState, useReducer, useEffect } from 'react';
 
 interface ValidationErrors<T> {
   [key: string]: T;
 }
 
-interface FormValues {
+export interface FormValues {
   [key: string]: string;
 }
 
@@ -16,14 +16,53 @@ type ValidationFunction = (
   values: Record<string, unknown>
 ) => void | ValidationErrors<string>;
 
+interface FormState {
+  values: FormValues;
+  errors: ValidationErrors<string>;
+  isValid: boolean;
+  touched: Touched;
+}
+
+interface UseFormReturn {
+  values: FormValues;
+  errors: ValidationErrors<string>;
+  isValid: boolean;
+  touched: Touched;
+  changeHandler: (event: any) => void;
+  submitHandler: (event: React.FormEvent<HTMLFormElement>) => void;
+  resetForm: () => void;
+}
+
+type FormAction =
+  | { type: 'SET_VALUES'; payload: FormValues }
+  | { type: 'SET_ERRORS'; payload: ValidationErrors<string> }
+  | { type: 'SET_IS_VALID'; payload: boolean }
+  | { type: 'SET_TOUCHED'; payload: Touched };
+
+function formReducer(state: FormState, action: FormAction): FormState {
+  switch (action.type) {
+    case 'SET_VALUES':
+      return { ...state, values: action.payload };
+    case 'SET_ERRORS':
+      return { ...state, errors: action.payload };
+    case 'SET_IS_VALID':
+      return { ...state, isValid: action.payload };
+    case 'SET_TOUCHED':
+      return { ...state, touched: action.payload };
+    default:
+      return state;
+  }
+}
+
 export function useForm(
   initialState: FormValues = {},
   validations: ValidationFunction[] = [],
   onSubmit: (
     event: React.FormEvent<HTMLFormElement>,
-    values: Record<string, string>
+    values: Record<string, string>,
+    resetForm: () => void
   ) => Promise<void>
-) {
+): UseFormReturn {
   function validate(
     validations: ValidationFunction[],
     values: Record<string, unknown>
@@ -47,34 +86,67 @@ export function useForm(
     validations,
     initialState
   );
-  const [values, setValues] = useState<{ [x: string]: string }>(initialState);
-  const [errors, setErrors] = useState(initialErrors);
-  const [isValid, setIsValid] = useState(initialIsValid);
-  const [touched, setTouched] = useState<{ [x: string]: boolean }>(
-    Object.keys(initialState).reduce((acc, key) => {
-      acc[key] = false;
-      return acc;
-    }, {} as Touched)
-  );
+  const [state, dispatch] = useReducer(formReducer, {
+    values: initialState,
+    errors: initialErrors,
+    isValid: initialIsValid,
+    touched: Object.keys(initialState).reduce(
+      (acc, key) => ({ ...acc, [key]: false }),
+      {}
+    ) as Touched,
+  });
+
+  const resetForm = (): void => {
+    dispatch({ type: 'SET_VALUES', payload: initialState });
+    dispatch({ type: 'SET_ERRORS', payload: {} });
+    dispatch({ type: 'SET_IS_VALID', payload: false });
+    dispatch({
+      type: 'SET_TOUCHED',
+      payload: Object.keys(initialState).reduce(
+        (acc, key) => ({ ...acc, [key]: false }),
+        {}
+      ) as Touched,
+    });
+  };
+
+  const initialValues = { ...initialState };
+  const [hasInitialized, setHasInitialized] = useState(false);
 
   const changeHandler = (event: { target: { name: any; value: any } }) => {
     const { name, value } = event.target;
-    const newValues = { ...values, [name]: value };
-    const newlyTouched = { ...touched, [name]: true };
+    const newValues = { ...state.values, [name]: value };
+    const newlyTouched = { ...state.touched, [name]: true };
     const { isValid, errors } = validate(validations, newValues);
-    setIsValid(isValid);
-    setValues(newValues);
-    setErrors(errors);
-    setTouched(newlyTouched);
+    dispatch({ type: 'SET_VALUES', payload: newValues });
+    dispatch({ type: 'SET_ERRORS', payload: errors });
+    dispatch({ type: 'SET_IS_VALID', payload: isValid });
+    dispatch({ type: 'SET_TOUCHED', payload: newlyTouched });
   };
 
   const submitHandler = async (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
-    if (!isValid) {
+    if (!state.isValid) {
       return;
     }
-    await onSubmit(event, values);
+    await onSubmit(event, state.values, resetForm);
   };
 
-  return { values, changeHandler, isValid, errors, touched, submitHandler };
+  useEffect(() => {
+    if (!hasInitialized) {
+      const { isValid, errors } = validate(validations, initialState);
+      dispatch({ type: 'SET_ERRORS', payload: errors });
+      dispatch({ type: 'SET_IS_VALID', payload: isValid });
+      setHasInitialized(true);
+    }
+  }, [hasInitialized, initialState, validations]);
+
+  return {
+    values: state.values,
+    changeHandler,
+    isValid: state.isValid,
+    errors: state.errors,
+    touched: state.touched,
+    submitHandler,
+    resetForm,
+  };
 }
--- src/components/contactForm/contactForm.componet.tsx
+++ src/components/contactForm/contactForm.componet.tsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import Button from '../button/button.componet';
-import { useForm } from '@/common/hooks/useForm';
+import { FormValues, useForm } from '@/common/hooks/useForm';
 import styles from './contactForm.module.css';
 
 type FormErrors = {
   [key: string]: string;
 };
 
-const initialState = {
+let initialState = {
   FirstName: '',
   LastName: '',
   Email: '',
@@ -49,7 +49,8 @@ const validations: ValidationFunction[] = [
 
 const onSubmit = async (
   event: React.FormEvent<HTMLFormElement>,
-  values: any
+  values: FormValues,
+  resetForm: () => void // Added resetForm function
 ) => {
   event.preventDefault();
 
@@ -63,9 +64,8 @@ const onSubmit = async (
     });
 
     if (response.ok) {
-      console.log('Email sent');
-    } else {
-      console.error('Something went wrong');
+      // Reset the form values to empty strings
+      resetForm();
     }
   } catch (error) {
     console.error(error);
@@ -73,77 +73,105 @@ const onSubmit = async (
 };
 
 export default function ContactForm() {
-  const { values, isValid, errors, changeHandler, touched, submitHandler } =
-    useForm(initialState, validations, onSubmit);
+  const {
+    values,
+    isValid,
+    errors,
+    changeHandler,
+    touched,
+    submitHandler,
+    resetForm,
+  } = useForm(initialState, validations, onSubmit);
   return (
-    <div className={styles.contactFormContainer}>
-      <form className={styles.form} onSubmit={submitHandler}>
-        <div className={styles.formControl}>
-          <label className={styles.label} htmlFor="FirstName">
-            first name
-          </label>
-          <input
-            onChange={changeHandler}
-            className={styles.input}
-            type="text"
-            name="FirstName"
-            id="FirstName"
-            placeholder="First Name"
-            aria-placeholder="first name"
-          />
-          {touched.FirstName && errors.FirstName && (
-            <p className={styles.validationWarning}>{errors.FirstName}</p>
-          )}
-        </div>
-        <div className={styles.formControl}>
-          <label className={styles.label} htmlFor="LastName">
-            last Name
-          </label>
-          <input
-            onChange={changeHandler}
-            className={styles.input}
-            type="text"
-            name="LastName"
-            id="LastName"
-            placeholder="Last Name"
-          />
-          {touched.LastName && errors.LastName && (
-            <p className={styles.validationWarning}>{errors.LastName}</p>
-          )}
-        </div>
-        <div className={styles.formControl}>
-          <label className={styles.label} htmlFor="Email">
-            email
-          </label>
-          <input
-            onChange={changeHandler}
-            className={styles.input}
-            type="email"
-            name="Email"
-            id="Email"
-            placeholder="Email@mail.com"
-          />
-          {touched.Email && errors.Email && (
-            <p className={styles.validationWarning}>{errors.Email}</p>
-          )}
-        </div>
-        <div className={styles.formControl}>
-          <label className={styles.label} htmlFor="comment">
-            comment
-          </label>
-          <textarea
-            onChange={changeHandler}
-            className={styles.input}
-            name="comment"
-            id="comment"
-            placeholder="Comment"
-          ></textarea>
-          {touched.comment && errors.comment && (
-            <p className={styles.validationWarning}>{errors.comment}</p>
-          )}
-        </div>
-        <Button type={'submit'} title={'Submit'} disabled={!isValid} />
-      </form>
+    <div
+      style={{
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+      }}
+    >
+      <h2 className={styles.title}>Contact me for a quote today</h2>
+      <div className={styles.contactFormContainer}>
+        <form className={styles.form} onSubmit={submitHandler}>
+          <div className={styles.formControl}>
+            <label className={styles.label} htmlFor="FirstName">
+              first name
+            </label>
+            <input
+              onChange={changeHandler}
+              className={styles.input}
+              value={values.FirstName}
+              type="text"
+              name="FirstName"
+              id="FirstName"
+              placeholder="First Name"
+              aria-placeholder="first name"
+            />
+            {touched.FirstName && errors.FirstName ? (
+              <p className={styles.validationWarning}>{errors.FirstName}</p>
+            ) : (
+              <div className={styles.validationWarningEmpty}></div>
+            )}
+          </div>
+          <div className={styles.formControl}>
+            <label className={styles.label} htmlFor="LastName">
+              last Name
+            </label>
+            <input
+              onChange={changeHandler}
+              className={styles.input}
+              value={values.LastName}
+              type="text"
+              name="LastName"
+              id="LastName"
+              placeholder="Last Name"
+            />
+            {touched.LastName && errors.LastName ? (
+              <p className={styles.validationWarning}>{errors.LastName}</p>
+            ) : (
+              <div className={styles.validationWarningEmpty}></div>
+            )}
+          </div>
+          <div className={styles.formControl}>
+            <label className={styles.label} htmlFor="Email">
+              email
+            </label>
+            <input
+              onChange={changeHandler}
+              className={styles.input}
+              value={values.Email}
+              type="email"
+              name="Email"
+              id="Email"
+              placeholder="Email@mail.com"
+            />
+            {touched.Email && errors.Email ? (
+              <p className={styles.validationWarning}>{errors.Email}</p>
+            ) : (
+              <div className={styles.validationWarningEmpty}></div>
+            )}
+          </div>
+          <div className={styles.formControl}>
+            <label className={styles.label} htmlFor="comment">
+              comment
+            </label>
+            <textarea
+              onChange={changeHandler}
+              className={styles.input}
+              value={values.comment}
+              name="comment"
+              id="comment"
+              placeholder="Comment"
+            ></textarea>
+            {touched.comment && errors.comment ? (
+              <p className={styles.validationWarning}>{errors.comment}</p>
+            ) : (
+              <div className={styles.validationWarningEmpty}></div>
+            )}
+          </div>
+          <Button type={'submit'} title={'Submit'} disabled={!isValid} />
+        </form>
+      </div>
     </div>
   );
 }
