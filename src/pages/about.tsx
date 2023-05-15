6d41f06d4d8c4779a2b9117f36e8230cbecd9225
Thomas Krogh <thomaskrogh@Thomass-Air.attlocal.net>
2023-05-14 22:07:13 -0500

    about page created

---
+++ src/pages/about.tsx
@@ -0,0 +1,96 @@
+import Layout from '@/components/layout/layout.component';
+import React from 'react';
+
+const About = () => {
+  return (
+    <Layout>
+      <div>
+        <h1>About</h1>
+        <section>
+          <h2>Who am I?</h2>
+          <p>
+            I'm am a frontend web developer focused on web page design and
+            accessibility. I specialize in React and Next.js and css libraries.
+            I mainly intend to work with small to medium sized companies.
+          </p>
+        </section>
+        <section>
+          <h2>What do I do?</h2>
+          <p>
+            I build websites and web applications. I also do some graphic
+            design. Keen focus on accessibility and user experience. Advertising
+            and marketing with a focus on SEO and SEM. content creation and
+            content management.
+          </p>
+        </section>
+        <section>
+          <h2>when did i start</h2>
+          <p>
+            I started in the year 2021 at the height of the covid-19 pandemic. I
+            entered a Google Ads course and found that I really enjoyed the web
+            development aspect of the course. I then decided to pursue a career
+            in web development. I was mentored under a professional web
+            developer and have been working on my own projects ever since.
+          </p>
+        </section>
+        <section>
+          <h2>where you can contact me</h2>
+          <p>
+            You can contact me via email:
+            <a href="mailto:thomas.krogh.dev@gmai.com">
+              <span>My Email Address</span>
+            </a>
+          </p>
+          <p>
+            You can also contact me via phone:
+            <a href="tel:1-801-347-9876">
+              <span>My Phone Number</span>
+            </a>
+          </p>
+          <p>
+            You can also contact me via social media:
+            <ul>
+              <li>
+                <a href="https://www.linkedin.com/in/thomas-krogh-0b1b3b1b9/">
+                  <span>My LinkedIn</span>
+                </a>
+              </li>
+              <li>
+                <a href="https://www.facebook.com/thomas.krogh.7">
+                  <span>My Facebook</span>
+                </a>
+              </li>
+              <li>
+                <a href="https://www.instagram.com/thomas_krogh_dev/">
+                  <span>My Instagram</span>
+                </a>
+              </li>
+            </ul>
+          </p>
+        </section>
+        <section>
+          <h2>why should you contact me</h2>
+          <p>
+            I am a hard worker and I am very passionate about what I do. I am
+            also very easy to work with and I am very flexible. I am also very
+            affordable and I am willing to work with you to find a price that
+            works for both of us.
+          </p>
+        </section>
+        <section>
+          <h2>how can i help you</h2>
+
+          <p>
+            I can help you with your web development needs. I can help you with
+            your web design needs. I can help you with your graphic design
+            needs. I can help you with your advertising and marketing needs. I
+            can help you with your content creation and content management
+            needs.
+          </p>
+        </section>
+      </div>
+    </Layout>
+  );
+};
+
+export default About;
